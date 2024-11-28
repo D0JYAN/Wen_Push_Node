47b35dd919b58d52d7418643bbc7640b7c4dfb2c
JIELSAN <jesussjg811@gmail.com>
2024-11-28 15:26:01 -0600

    creacion de index/serve

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Importamos la libreria de express
+const express = require('express');
+//Importar morgan
+const morgan = require('morgan');
+//Ejecutar el modulo de express
+const app = express();
+
+//Middlewares
+
+
+//Routes
+
+//Static Content
