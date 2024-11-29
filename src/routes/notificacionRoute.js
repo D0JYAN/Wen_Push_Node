f8f83f07c9457ada846a9353f596a88315bf17a0
JIELSAN <jesussjg811@gmail.com>
2024-11-28 23:58:24 -0600

    endpoint de la subscripcion (post)

--- src/routes/notificacionRoute.js
+++ src/routes/notificacionRoute.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const webPush = require('../webPush');
+
+//Guardar datos que llegan
+let pushSubscription;
+
+//Endpoints
+//subscripcion
+router.post('/subscripcion', async (req, res) => {
+    pushSubscription = req.body;
+    console.log(req.body);
+    res.status(200).json();
+    //Datos a enviar
+    const data = JSON.stringify({
+        title: 'Nueva notificación',
+        message: 'Esta es una nueva notificación'
+    })
+    //Enviar la notificacion
+    try {
+        await webPush.sendNotification(pushSubscription, data);
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = router;
