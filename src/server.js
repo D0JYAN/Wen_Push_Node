184cb724f9f09e4796f26ee471d6948be70fab3b
JIELSAN <jesussjg811@gmail.com>
2024-11-28 22:34:43 -0600

    creacion del server

--- src/server.js
+++ src/server.js
@@ -2,12 +2,30 @@
 const express = require('express');
 //Importar morgan
 const morgan = require('morgan');
+//Importar body-parser
+const bodyParser = require('body-parser');
+//Importacion de path
+const path = require('node:path');
 //Ejecutar el modulo de express
 const app = express();
 
-//Middlewares
+//Importacion de Routes
+const notificacionRoute = require('./routes/notificacionRoute');
 
+//Middlewares
+app.use(morgan('dev'));
+// app.use(express.urlencoded( { extended: false } ));//Leer los datos que se pasan en la url
+app.use(bodyParser.json());//Convertir los datos que llegan a JSON
 
 //Routes
 
+
 //Static Content
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Iniciar servidor
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
